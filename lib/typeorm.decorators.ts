0a68e90b573f9a78e881b23108a0827fd7b66146
Florian Zysset <florian@zysset.me>
2018-05-18 17:32:55 +0100

    feat: add @InjectedEventSubscriber to support injection from nestjs

--- lib/typeorm.decorators.ts
+++ lib/typeorm.decorators.ts
@@ -1,6 +1,9 @@
 import { Inject } from '@nestjs/common';
 import { Connection, ConnectionOptions } from 'typeorm';
 
+const AUTO_DELCARED_DEPS_METADATA = 'design:paramtypes';
+const SELF_DECLARED_DEPS_METADATA = 'self:paramtypes';
+
 import {
   getRepositoryToken,
   getConnectionToken,
@@ -21,3 +24,48 @@ export const InjectEntityManager: (
 ) => ParameterDecorator = (
   connection?: Connection | ConnectionOptions | string,
 ) => Inject(getEntityManagerToken(connection));
+
+export function InjectedEventSubscriber(
+  connection?: Connection | ConnectionOptions | string,
+): ClassDecorator {
+  return (target: any) => {
+    const original = target;
+
+    const f: any = function(...args) {
+      const conn = args[0] as Connection;
+      let instance = new original(...args.slice(1));
+      Object.assign(conn, {
+        subscribers: [].concat(conn.subscribers).concat([instance]),
+      });
+      console.log(f);
+      return instance;
+    };
+
+    f.prototype = original.prototype;
+
+    // Apply metadata from design type for NestJS auto-injection, and add the default Connection
+    const argsMetadata =
+      Reflect.getMetadata(AUTO_DELCARED_DEPS_METADATA, target) || [];
+    const newArgsMetadata = [Connection, ...argsMetadata];
+    Reflect.defineMetadata(AUTO_DELCARED_DEPS_METADATA, newArgsMetadata, f);
+
+    // Apply metadata from Token @Inject, and append Connection request as parameter to the decorator
+    const manualMetadata =
+      Reflect.getMetadata(SELF_DECLARED_DEPS_METADATA, target) || [];
+    const newManualMetatadata = [
+      ...manualMetadata.map((meta: any) =>
+        Object.assign({}, meta, { index: meta.index + 1 }),
+      ),
+    ];
+    if (connection) {
+      newManualMetatadata.push({
+        index: 0,
+        param: getConnectionToken(connection),
+      });
+    }
+    Reflect.defineMetadata(SELF_DECLARED_DEPS_METADATA, newManualMetatadata, f);
+
+    // return new constructor (will override original)
+    return f;
+  };
+}
