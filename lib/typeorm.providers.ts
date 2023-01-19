76999b2ab1f1ac93d51a34c02c8a4d9a660ee45c
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2023-01-19 15:32:56 -0400

    refactor: improve type def on `createTypeOrmProviders`

--- lib/typeorm.providers.ts
+++ lib/typeorm.providers.ts
@@ -1,22 +1,30 @@
-import { Provider } from '@nestjs/common';
-import { DataSource, DataSourceOptions, getMetadataArgsStorage } from 'typeorm';
+import { FactoryProvider } from '@nestjs/common';
+import {
+  DataSource,
+  DataSourceOptions,
+  getMetadataArgsStorage,
+  Repository,
+  TreeRepository,
+} from 'typeorm';
 import { getDataSourceToken, getRepositoryToken } from './common/typeorm.utils';
 import { EntityClassOrSchema } from './interfaces/entity-class-or-schema.type';
 
 export function createTypeOrmProviders(
   entities?: EntityClassOrSchema[],
   dataSource?: DataSource | DataSourceOptions | string,
-): Provider[] {
+): FactoryProvider<TreeRepository<any> | Repository<any>>[] {
   return (entities || []).map((entity) => ({
     provide: getRepositoryToken(entity, dataSource),
     useFactory: (dataSource: DataSource) => {
-      const enitityMetadata = dataSource.entityMetadatas.find((meta) => meta.target === entity)
-      const isTreeEntity = typeof enitityMetadata?.treeType !== 'undefined'
-      return isTreeEntity 
+      const enitityMetadata = dataSource.entityMetadatas.find(
+        (meta) => meta.target === entity,
+      );
+      const isTreeEntity = typeof enitityMetadata?.treeType !== 'undefined';
+      return isTreeEntity
         ? dataSource.getTreeRepository(entity)
         : dataSource.options.type === 'mongodb'
-          ? dataSource.getMongoRepository(entity)
-          : dataSource.getRepository(entity);
+        ? dataSource.getMongoRepository(entity)
+        : dataSource.getRepository(entity);
     },
     inject: [getDataSourceToken(dataSource)],
     /**
