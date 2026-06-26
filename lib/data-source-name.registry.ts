d667e3fa8d49b4ba7374d6c7453a28092330e4b2
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2026-06-26 09:52:49 +0200

    chore: temp check ci fail

--- lib/data-source-name.registry.ts
+++ lib/data-source-name.registry.ts
@@ -1,5 +1,3 @@
-import { Logger } from '@nestjs/common';
-
 /**
  * Tracks active data source names at runtime in order to prevent multiple
  * data sources from being registered under the same name. Registering two
@@ -8,19 +6,19 @@ import { Logger } from '@nestjs/common';
  * of hard-to-debug configuration issues.
  */
 export class DataSourceNameRegistry {
-  private static readonly logger = new Logger(DataSourceNameRegistry.name);
+  // private static readonly logger = new Logger(DataSourceNameRegistry.name);
   private static readonly registeredNames = new Set<string>();
 
   static register(dataSourceName: string): void {
     if (this.registeredNames.has(dataSourceName)) {
       // Commented out to avoid throwing an exception as it caused regressions in test suites.
       // throw new DuplicateDataSourceException(dataSourceName);
-      this.logger.warn(
-        `A data source with the ${dataSourceName} name is already registered. ` +
-          `Multiple data sources must be registered with unique names; otherwise, ` +
-          `they will override each other. Please assign a unique "name" property ` +
-          `to each TypeOrmModule.forRoot()/forRootAsync() registration.`,
-      );
+      // this.logger.warn(
+      //   `A data source with the ${dataSourceName} name is already registered. ` +
+      //     `Multiple data sources must be registered with unique names; otherwise, ` +
+      //     `they will override each other. Please assign a unique "name" property ` +
+      //     `to each TypeOrmModule.forRoot()/forRootAsync() registration.`,
+      // );
     }
     this.registeredNames.add(dataSourceName);
   }
